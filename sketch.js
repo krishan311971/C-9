8153144beacc5d80c66e920455b0690c00f6cad9
krishan311971 <99807239+krishan311971@users.noreply.github.com>
2022-02-20 22:33:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,48 @@
+var sea,ship;
+var seaImg,shipImg;
 
-function setup() {
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+   //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
   createCanvas(400,400);
-  background(5100);
-  box = createSprite(200,200,30,40);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
-function draw() 
-{
-
-  // write the code to change background color 
-background("white");
-  // to red when RIGHT_ARROW is pressed
+function draw() {
+  background(0);
+  sea.velocityX = -3;
  
-if(keyIsDown(RIGHT_ARROW)){
-  background("red");
-}
- // to yellow when LEFT_ARROW is pressed
- if(keyIsDown(LEFT_ARROW)){
-   background("yellow");
- }
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
  
   drawSprites();
 }
-
